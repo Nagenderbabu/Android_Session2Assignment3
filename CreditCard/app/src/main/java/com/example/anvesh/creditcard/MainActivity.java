b809e740b997ee3ddc999e22bf10bb7526a2b8ea
Nagenderbabu <nagenderbabu34@gmail.com>
2017-05-16 16:02:09 +0530

    Re-Submitted credit card helper

--- CreditCard/app/src/main/java/com/example/anvesh/creditcard/MainActivity.java
+++ CreditCard/app/src/main/java/com/example/anvesh/creditcard/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.anvesh.creditcard;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,19 +11,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // created relative layout is passed to content view
-        setContentView(R.layout.relativelayout);
+        // created linear layout is included in main activity view
+        // setting activity_main to content view
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
